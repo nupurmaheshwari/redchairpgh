a5e1a45d4283436e5a873baa2e910fd889220ba8
Jenny Zhu <jwzhu@andrew.cmu.edu>
2019-04-24 22:59:23 +0000

    add attrs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,25 @@
 class SessionsController < ApplicationController
   
   def create
-    puts request.env['omniauth.auth']
     begin
-      auth_hash = request.env['omniauth.auth']
-      puts auth_hash
-      @user = User.from_omniauth(auth_hash)
-      if @user.nil? 
-        puts "GOOD no user profile for you yet!"
-        @user = User.new(uid: auth_hash['uid'], provider: auth_hash['provider'], 
-        role: 'contributor', first_name: auth_hash['info']['first_name'], 
-        last_name: auth_hash['info']['last_name'], image_url: auth_hash['info']['picture_url'],
-        code_of_conduct: false, active: false, is_new: true)
-        @user.save 
+      if params[:commit]
+        @user = User.find_by_username(params[:email]) 
+        puts @user.attributes 
+        puts "haha"
+      else
+        auth_hash = request.env['omniauth.auth']
+        @user = User.from_omniauth(auth_hash)
+        if @user.nil? 
+          puts "GOOD no user profile for you yet!"
+          @user = User.new(uid: auth_hash['uid'], provider: auth_hash['provider'], 
+          role: 'contributor', first_name: auth_hash['info']['first_name'], 
+          last_name: auth_hash['info']['last_name'], image_url: auth_hash['info']['picture_url'],
+          code_of_conduct: false, active: false, is_new: true)
+          @user.save 
+        end
+        session[:user_id] = @user.id
+        flash[:success] = "Welcome, #{@user.first_name}!"
       end
-      session[:user_id] = @user.id
-      flash[:success] = "Welcome, #{@user.first_name}!"
     rescue
     flash[:warning] = "There was an error while trying to authenticate you..."
     end
