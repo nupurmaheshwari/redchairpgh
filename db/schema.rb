d77b1b4c0bd540bb085c4951fa6a2c57617919c7
Nupur Maheshwari <nupurm@andrew.cmu.edu>
2019-04-25 23:43:25 -0400

    Removed duplicate agreed migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190425230510) do
+ActiveRecord::Schema.define(version: 20190425230254) do
 
   create_table "mentees", force: :cascade do |t|
     t.boolean "is_active"
@@ -72,14 +72,17 @@ ActiveRecord::Schema.define(version: 20190425230510) do
     t.string "location"
     t.text "image_url"
     t.string "url"
+    t.boolean "agreed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "is_new"
     t.boolean "code_of_conduct"
     t.boolean "active"
     t.string "password_digest"
-    t.datetime "updated_at"
-    t.datetime "created_at"
     t.string "username"
-    t.boolean "agreed"
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
+    t.index ["provider"], name: "index_users_on_provider"
+    t.index ["uid"], name: "index_users_on_uid"
   end
 
 end
