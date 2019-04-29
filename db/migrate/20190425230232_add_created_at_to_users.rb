57443f814209e3629e0bd79e5edd0e7f5d688e43
Sruthi Surapaneni <sssurapa@andrew.cmu.edu>
2019-04-29 19:18:18 -0400

    account

--- db/migrate/20190425230232_add_created_at_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddCreatedAtToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :created_at, :datetime
-  end
-end
--- db/migrate/20190425230510_add_agreed_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddAgreedToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :agreed, :boolean
-  end
-end
